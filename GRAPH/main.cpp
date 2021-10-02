52d2bfe129127426e40e9247003cf60d94a29250
Anaritus <61704392+Anaritus@users.noreply.github.com>
2021-10-02 18:06:14 +0300

    Add files via upload

--- GRAPH/main.cpp
+++ GRAPH/main.cpp
@@ -1,4 +1,4 @@
-#ifdef WIN32
+﻿#ifdef WIN32
 #include <SDL.h>
 #undef main
 #else
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <map>
 
 std::string to_string(std::string_view str)
 {
@@ -31,33 +32,40 @@ void glew_fail(std::string_view message, GLenum error)
 const char vertex_shader_source[] =
 R"(#version 330 core
 uniform mat4 view;
-layout (location = 0) in vec2 in_position;
+uniform mat4 transform;
+layout (location = 0) in vec3 in_position;
 layout (location = 1) in vec4 in_color;
-layout (location = 2) in float in_dist;
 out vec4 color;
-out vec2 position;
-out float dist;
 void main()
 {
-	gl_Position = view * vec4(in_position, 0.0, 1.0);
+	gl_Position = view * transform * vec4(in_position, 1.0);
 	color = in_color;
-	dist = in_dist;
 }
 )";
 
 const char fragment_shader_source[] =
 R"(#version 330 core
 in vec4 color;
-in float dist;
-in vec2 position;
 layout (location = 0) out vec4 out_color;
-uniform int dash;
 void main()
 {
-		if (dash == 1 && mod(dist, 40.0) < 20.0) discard;
-		out_color = color;
+	out_color = color;
 }
 )";
+
+const char vertex_shader_source_2D[] =
+R"(#version 330 core
+uniform mat4 view;
+layout (location = 0) in vec3 in_position;
+layout (location = 1) in vec4 in_color;
+out vec4 color;
+void main()
+{
+	gl_Position = view * vec4(in_position, 1.0);
+	color = in_color;
+}
+)";
+
 
 GLuint create_shader(GLenum type, const char* source)
 {
@@ -98,6 +106,13 @@ GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
 	return result;
 }
 
+struct vec3
+{
+	float x;
+	float y;
+	float z;
+};
+
 struct vec2
 {
 	float x;
@@ -106,56 +121,88 @@ struct vec2
 
 struct vertex
 {
-	vec2 position;
+	vec3 position;
 	std::uint8_t color[4];
-	float d = 0;
+};
 
-	vertex()
+struct funcy
+{
+	std::vector<vec2> roots;
+	void newRoot(vec2 root)
 	{
-		position.x = rand() % 500; // * 1.0 / RAND_MAX; 
-		position.y = rand() % 500; //  1.0 / RAND_MAX;
-		color[0] = 120;
-		color[1] = 111;
-		color[2] = 90;
-		color[3] = 1;
+		roots.push_back(root);
 	}
-
-	vertex(int x, int y)
+	vertex apply(vec2 point)
 	{
-		position.x = x; // * 1.0 / RAND_MAX; 
-		position.y = y; //  1.0 / RAND_MAX;
-		color[0] = 120 * x;
-		color[1] = 111 * y;
-		color[2] = 90 * (x + y);
-		color[3] = 1;
-	}
-	vertex(vec2 p)
-	{
-		position.x = p.x; // * 1.0 / RAND_MAX; 
-		position.y = p.y; //  1.0 / RAND_MAX;
-		color[0] = 255;
-		color[1] = 255;
-		color[2] = 255;
-		color[3] = 1;
+		float retZ = 0;
+		for (auto root : roots) {
+			retZ += cos(std::hypot(root.x - point.x, root.y - point.y)) / std::hypot(root.x - point.x, root.y - point.y);
+		}
+		retZ = retZ / roots.size();
+		std::uint8_t col = static_cast<uint8_t>((retZ + 1) * 128);
+		return { {point.x, point.y, retZ}, {col, static_cast <uint8_t>(col / 2), static_cast<uint8_t>(-col), 255} };
 	}
 };
 
-vec2 bezier(std::vector<vertex> const& vertices, float t)
+static vertex cube_vertices[]
 {
-	std::vector<vec2> points(vertices.size());
+	// -X
+	{{-1.f, -1.f, -1.f}, {  255, 255, 255, 255}},
+	{{-1.f, -1.f,  1.f}, {  255, 255, 255, 255}},
+	{{-1.f,  1.f, -1.f}, {  255, 255, 255, 255}},
+	{{-1.f,  1.f,  1.f}, {  255, 255, 255, 255}},
+	// -Y
+	{{-1.f, -1.f, -1.f}, {255,   255, 255, 255}},
+	{{ 1.f, -1.f, -1.f}, {255,   255, 255, 255}},
+	{{-1.f, -1.f,  1.f}, {255,   255, 255, 255}},
+	{{ 1.f, -1.f,  1.f}, {255,   255, 255, 255}},
+	// -Z
+	{{ 1.f, -1.f, -1.f}, {255, 255,   255, 255}},
+	{{-1.f, -1.f, -1.f}, {255, 255,   255, 255}},
+	{{ 1.f,  1.f, -1.f}, {255, 255,   255, 255}},
+	{{-1.f,  1.f, -1.f}, {255, 255,   255, 255}}
+};
 
-	for (std::size_t i = 0; i < vertices.size(); ++i)
-		points[i] = vertices[i].position;
+static vertex axises[]
+{
+	{{-0.99f,  1.f, -0.99f}, {0, 0, 0, 0}}, //x
+	{{1.f,  -0.99f, -0.99f}, {0, 0, 0, 0}}, //y
+	{{-0.99f,  -0.99f, 1.f}, {0, 0, 0, 0}}, //z
+	{{-0.99f,  -0.99f, -0.99f}, {0, 0, 0, 0}} //0
+};
 
-	// De Casteljau's algorithm
-	for (std::size_t k = 0; k + 1 < vertices.size(); ++k) {
-		for (std::size_t i = 0; i + k + 1 < vertices.size(); ++i) {
-			points[i].x = points[i].x * (1.f - t) + points[i + 1].x * t;
-			points[i].y = points[i].y * (1.f - t) + points[i + 1].y * t;
-		}
-	}
-	return points[0];
-}
+static vertex input_square[]
+{
+	{{1.5f, -0.6f, 0.f}, {255, 255,   255, 255}}, //+-
+	{{0.9f, -0.6f, 0.f}, {255, 255,   255, 255}}, //--
+	{{1.5f, 0.f, 0.f}, {255, 255,   255, 255}}, //++
+	{{0.9f, 0.f, 0.f}, {255, 255,   255, 255}} //-+
+};
+
+static std::uint32_t cube_indices[]
+{
+	// -X
+	0, 1, 2, 2, 1, 3,
+
+	// -Y
+	4, 5, 6, 6, 5, 7,
+
+	// -Z
+	8, 9, 10, 10, 9, 11
+};
+
+static std::uint32_t vert_indices[]
+{
+	0, 3,
+	1, 3,
+	2
+};
+
+static std::uint32_t is_indices[]
+{
+	1, 0, 2,
+	2, 1, 3
+};
 
 
 
@@ -170,8 +217,12 @@ int main() try
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-	SDL_Window* window = SDL_CreateWindow("Graphics course practice 3",
+	SDL_Window* window = SDL_CreateWindow("Graphics course practice 4",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
 		800, 600,
@@ -187,8 +238,6 @@ int main() try
 	if (!gl_context)
 		sdl2_fail("SDL_GL_CreateContext: ");
 
-	SDL_GL_SetSwapInterval(0);
-
 	if (auto result = glewInit(); result != GLEW_NO_ERROR)
 		glew_fail("glewInit: ", result);
 
@@ -198,63 +247,46 @@ int main() try
 	glClearColor(0.f, 0.f, 0.f, 0.f);
 
 	auto vertex_shader = create_shader(GL_VERTEX_SHADER, vertex_shader_source);
+	auto vertex_shader_2D = create_shader(GL_VERTEX_SHADER, vertex_shader_source_2D);
 	auto fragment_shader = create_shader(GL_FRAGMENT_SHADER, fragment_shader_source);
 	auto program = create_program(vertex_shader, fragment_shader);
+	auto program_2D = create_program(vertex_shader_2D, fragment_shader);
 
 	GLuint view_location = glGetUniformLocation(program, "view");
-	GLuint dash_location = glGetUniformLocation(program, "dash");
+	GLuint view_location_2D = glGetUniformLocation(program_2D, "view");
+	GLuint transform_location = glGetUniformLocation(program, "transform");
 
 	auto last_frame_start = std::chrono::high_resolution_clock::now();
 
 	std::vector<vertex> mass;
-	std::vector<float> d;
-	std::vector<vertex> bezps;
-	int quality = 4;
 
-	GLuint vbo, vbeo;
-	GLuint vao, vabeo;
+	GLuint vbo, vao, ebo;
 	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glGenBuffers(1, &ebo);
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)(0));
-
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)(0));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(vertex), (void*)(sizeof(vec2)));
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)(sizeof(vec2) + 4 * sizeof(uint8_t)));
-
-
-	glGenBuffers(1, &vbeo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbeo);
-	glGenVertexArrays(1, &vabeo);
-	glBindVertexArray(vabeo);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)(0));
-
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(vertex), (void*)(sizeof(vec2)));
-
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)(sizeof(vec2) + 4 * sizeof(uint8_t)));
-	glPointSize(10);
-
-	//glGenVertexArrays(1, &vabeo);
-	//glBindVertexArray(vabeo);
-	//glEnableVertexAttribArray(2);
-	//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)(0));
-
-	//glEnableVertexAttribArray(3);
-	//glVertexAttribPointer(3, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(vertex), (void*)(sizeof(vec2)));
+	glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(vertex), (void*)(sizeof(vec3)));
 
 	float time = 0.f;
+	float angle = 0.f;
+	float speed = 1.f;
+	float cube_x = -0.9f, cube_y = -0.9f;
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	//glEnable(GL_CULL_FACE);
+	glCullFace(GL_FRONT);
+
+	std::map<SDL_Keycode, bool> button_down;
 
 	bool running = true;
 	while (running)
 	{
-
-
 		for (SDL_Event event; SDL_PollEvent(&event);) switch (event.type)
 		{
 		case SDL_QUIT:
@@ -272,91 +304,27 @@ int main() try
 		case SDL_MOUSEBUTTONDOWN:
 			if (event.button.button == SDL_BUTTON_LEFT)
 			{
-				int mouse_x = event.button.x;
-				int mouse_y = event.button.y;
-				mass.push_back(vertex(mouse_x, mouse_y));
-				mass[mass.size() - 1].d = 0;
-				d.clear();
-				vec2 prev, cur;
-				bezps.clear();
-				if (!mass.empty()) {
-					for (int i = 0; i <= (mass.size() - 1) * quality + 1; i++) {
-						bezps.push_back(vertex(bezier(mass, i * 1.0f / ((mass.size() - 1) * quality + 1))));
-						if (!i) prev = bezps[0].position;
-						else prev = cur;
-						cur = bezps[bezps.size() - 1].position;
-						if (i)
-							d.push_back(std::hypot(cur.x - prev.x, cur.y - prev.y) + d[d.size() - 1]);
-						else
-							d.push_back(std::hypot(cur.x - prev.x, cur.y - prev.y));
-						bezps[bezps.size() - 1].d = d[d.size() - 1];
-					}
+				float mouse_x = 2.f / width * event.button.x - 1;
+				mouse_x = mouse_x * width / height;
+				float mouse_y = -2.f / height * event.button.y + 1;
+				if (0.9f <= mouse_x && mouse_x <= 1.5f &&
+					-0.6f <= mouse_y && mouse_y <= 0.f)
+				{
+					mass.push_back({ {mouse_x, mouse_y, 0.f}, {  200, 0, 200, 255} });
 				}
 			}
 			else if (event.button.button == SDL_BUTTON_RIGHT)
 			{
 				if (!mass.empty()) {
 					mass.pop_back();
-					d.clear();
-					vec2 prev, cur;
-					bezps.clear();
-					if (!mass.empty()) {
-						for (int i = 0; i <= (mass.size() - 1) * quality + 1; i++) {
-							bezps.push_back(vertex(bezier(mass, i * 1.0f / ((mass.size() - 1) * quality + 1))));
-							if (!i) prev = bezps[0].position;
-							else prev = cur;
-							cur = bezps[bezps.size() - 1].position;
-							if (i)
-								d.push_back(std::hypot(cur.x - prev.x, cur.y - prev.y) + d[d.size() - 1]);
-							else
-								d.push_back(std::hypot(cur.x - prev.x, cur.y - prev.y));
-							bezps[bezps.size() - 1].d = d[d.size() - 1];
-						}
-					}
 				}
 			}
 			break;
 		case SDL_KEYDOWN:
-			if (event.key.keysym.sym == SDLK_LEFT)
-			{
-				quality = std::max(1, quality - 1);
-				d.clear();
-				vec2 prev, cur;
-				bezps.clear();
-				if (!mass.empty()) {
-					for (int i = 0; i <= (mass.size() - 1) * quality + 1; i++) {
-						bezps.push_back(vertex(bezier(mass, i * 1.0f / ((mass.size() - 1) * quality + 1))));
-						if (!i) prev = bezps[0].position;
-						else prev = cur;
-						cur = bezps[bezps.size() - 1].position;
-						if (i)
-							d.push_back(std::hypot(cur.x - prev.x, cur.y - prev.y) + d[d.size() - 1]);
-						else
-							d.push_back(std::hypot(cur.x - prev.x, cur.y - prev.y));
-						bezps[bezps.size() - 1].d = d[d.size() - 1];
-					}
-				}
-			}
-			else if (event.key.keysym.sym == SDLK_RIGHT)
-			{
-				quality++;
-				d.clear();
-				vec2 prev, cur;
-				bezps.clear();
-				if (!mass.empty()) {
-					for (int i = 0; i <= (mass.size() - 1) * quality + 1; i++) {
-						bezps.push_back(vertex(bezier(mass, i * 1.0f / ((mass.size() - 1) * quality + 1))));
-						if (!i) prev = bezps[0].position;
-						else prev = cur;
-						cur = bezps[bezps.size() - 1].position;
-						if (i)
-							d.push_back(std::hypot(cur.x - prev.x, cur.y - prev.y) + d[d.size() - 1]);
-						else
-							d.push_back(std::hypot(cur.x - prev.x, cur.y - prev.y));
-						bezps[bezps.size() - 1].d = d[d.size() - 1];
-					}
-				}
-			}
+			button_down[event.key.keysym.sym] = true;
+			break;
+		case SDL_KEYUP:
+			button_down[event.key.keysym.sym] = false;
 			break;
 		}
 
@@ -369,40 +337,62 @@ int main() try
 		time += dt;
 
 		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_DEPTH_BUFFER_BIT);
+
+		if (button_down[SDLK_LEFT]) angle -= dt * speed;
+		if (button_down[SDLK_RIGHT]) angle += dt * speed;
+
+		float near = 0.1f, far = 10.f;
+		float right = 1.1f * near, top = 1.1f * near * height / width;
 
 		float view[16] =
 		{
-			2.f / width, 0.f, 0.f, -1.f,
-			0.f, -2.f / height, 0.f, 1.f,
-			0.f, 0.f, 1.f, 0.f,
-			0.f, 0.f, 0.f, 1.f,
+			near / right, 0.f, 0.f, 0.f,
+			0.f, near / top, 0.f, 0.f,
+			0.f, 0.f, -(far + near) / (far - near), -(2 * far * near) / (far - near),
+			0.f, 0.f, -1.f, 0,
+		};
+
+		float view_2D[16] =
+		{
+			(1.f * height) / width, 0, 0, 0,
+			0, 1.f, 0, 0,
+			0, 0, 1.f, 0,
+			0, 0, 0, 1.f
+		};
+
+
+		float scale = 1.f;
+		float transform[16] =
+		{
+			cos(angle) * scale, 0.f, -sin(angle) * scale, cube_x,
+			0.f, scale, 0.f, cube_y,
+			sin(angle) * scale, 0.f, cos(angle) * scale, -5.f,
+			0.f, 0.f, 0.f, 1.f
 		};
 
 		glUseProgram(program);
 		glUniformMatrix4fv(view_location, 1, GL_TRUE, view);
 
-		if (mass.size() != 0)
-		{
-			glUniform1i(dash_location, 0);
-			glBindBuffer(GL_ARRAY_BUFFER, vbo);
-			glBindVertexArray(vao);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(mass[0]) * mass.size(), mass.data(), GL_STATIC_COPY);
-			glDrawArrays(GL_LINE_STRIP, 0, mass.size());
-			glDrawArrays(GL_POINTS, 0, mass.size());
-		}
+		glUniformMatrix4fv(transform_location, 1, GL_TRUE, transform);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), &cube_vertices, GL_STATIC_COPY);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), &cube_indices, GL_STATIC_COPY);
+		glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
 
 
-		if (bezps.size() != 0)
-		{
-			glUniform1i(dash_location, 1);
-			glBindBuffer(GL_ARRAY_BUFFER, vbeo);
-			glBindVertexArray(vabeo);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(bezps[0]) * bezps.size(), bezps.data(), GL_STATIC_COPY);
-			glDrawArrays(GL_LINE_STRIP, 0, bezps.size());
-		}
+		glBufferData(GL_ARRAY_BUFFER, sizeof(axises), &axises, GL_STATIC_COPY);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vert_indices), &vert_indices, GL_STATIC_COPY);
+		glDrawElements(GL_LINE_STRIP, 5, GL_UNSIGNED_INT, 0);
 
 
+		glUseProgram(program_2D);
+		glUniformMatrix4fv(view_location_2D, 1, GL_TRUE, view_2D);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(input_square), &input_square, GL_STATIC_COPY);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(is_indices), &is_indices, GL_STATIC_COPY);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * mass.size(), mass.data(), GL_STATIC_COPY);
+		glDrawArrays(GL_LINE_STRIP, 0, mass.size());
 
 		SDL_GL_SwapWindow(window);
 	}
